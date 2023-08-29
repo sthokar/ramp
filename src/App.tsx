e9f33c3ba93bb6f1644cad909163dcbea7f73eda
sthokar <thokarsameer@yahoo.com>
2023-08-28 21:52:25 -0400

    bug 3 error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -64,8 +64,14 @@ export function App() {
             if (newValue === null) {
               return
             }
-
-            await loadTransactionsByEmployee(newValue.id)
+           
+            if (newValue.id){
+              await loadTransactionsByEmployee(newValue.id)
+            }
+            else{
+              await loadAllTransactions()
+            }
+           
           }}
         />
 
